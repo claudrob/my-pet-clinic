370150a3a54fa85a14e8c1c07ed06afa734170e2
claudrob <crobinson@tec.mx>
2023-10-21 22:45:51 -0600

    Add base Service Interface. Closes #11

--- my-pet-clinic-data/src/main/java/my/edu/services/VetService.java
+++ my-pet-clinic-data/src/main/java/my/edu/services/VetService.java
@@ -4,12 +4,6 @@ import my.edu.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
 
-    Vet findById(Long id);
-
-    Vet save(Vet Vet);
-
-    Set<Vet> findAll();
-    
 }
