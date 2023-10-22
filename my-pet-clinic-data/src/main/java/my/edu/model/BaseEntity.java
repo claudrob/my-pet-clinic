2fa4e8cdfd58e70a101cf56d6b79f257d1a3834d
claudrob <crobinson@tec.mx>
2023-10-22 14:34:04 -0600

    Updated Owner Controller. Closes #16

--- my-pet-clinic-data/src/main/java/my/edu/model/BaseEntity.java
+++ my-pet-clinic-data/src/main/java/my/edu/model/BaseEntity.java
@@ -1,9 +1,15 @@
 package my.edu.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.io.Serializable;
 
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
--- my-pet-clinic-web/src/main/java/my/edu/controllers/OwnerController.java
+++ my-pet-clinic-web/src/main/java/my/edu/controllers/OwnerController.java
@@ -1,14 +1,30 @@
 package my.edu.controllers;
 
+import my.edu.model.Owner;
+import my.edu.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwner(){
+    public String listOwner(Model model){
+
+        Set<Owner> set = ownerService.findAll();
+        for(Owner o : set)
+            System.out.println("OwnerController: " + o);
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
